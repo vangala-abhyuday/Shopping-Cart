ce523f102d2d0515296e0f150c9b06d1b47b30f5
abhi <abhyuday1729@gmail.com>
2021-10-23 21:17:43 +0530

    collectstatic

--- mac/settings.py
+++ mac/settings.py
@@ -17,6 +17,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
