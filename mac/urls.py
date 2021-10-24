2c9a1e04a65ef241a98eb9cd8880a9f7dbc4b486
abhi <abhyuday1729@gmail.com>
2021-10-24 18:40:03 +0530

    resolved errors

--- mac/urls.py
+++ mac/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from . import views
 from django.views.generic import TemplateView
 
+
 from django.views.static import serve
 from django.conf.urls import url
 
